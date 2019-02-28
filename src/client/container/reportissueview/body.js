6f8fe18c1c156003351a2d28b81ea82fb519fe06
pallab-gain <pallab@callstats.io>
2019-02-28 14:24:17 +0200

    Report a issue page.

--- src/client/container/reportissueview/body.js
+++ src/client/container/reportissueview/body.js
@@ -2,12 +2,16 @@ import React, {Component} from "react";
 import {connect} from "react-redux";
 import PropTypes from "prop-types";
 import SVG from 'react-inlinesvg';
+import lo from 'lodash';
 
 import closeOrDismissIcon from '../../res/images/fa-close-or-dismiss.svg';
 import starYellowIcon from '../../res/images/star-yellow.svg';
 import starWhiteIcon from '../../res/images/star-white.svg';
+import markFalseIcon from '../../res/images/fa-mark-false.svg';
+import markTrueIcon from '../../res/images/fa-mark-true.svg';
 
 import {
+	onFeedbackChange,
 	onRequestReportCallIssue
 } from "../../reducers/acReducer";
 
@@ -20,26 +24,32 @@ class Body extends Component {
 		super(props);
 	}
 
+	feedbackChange(currentFeedback) {
+		this.props.feedbackChange(currentFeedback);
+	}
+
 	requestReportACallIssue(issueObject) {
 		this.props.requestReportACallIssue();
 	}
 
-	closeSetting() {
+	closeReportCallIssue() {
 		this.props.closeReportCallIssue();
 	}
 
 	render() {
-		const feedback = defaultFeedback;
+		const feedback = this.props.feedback;
 		return (
-			<div className="card-body" style={{ backgroundColor: '#ffffff'}}>
+			<div className="card-body" style={{backgroundColor: '#ffffff'}}>
 				<div className="row ">
 					<div className="col-md-10">
 						<p style={{color: '#000000', fontSize: '18px', fontFamily: 'AmazonEmber'}}>Report a
 							call issue</p>
 					</div>
-					<div className="col-md-2"><SVG src={closeOrDismissIcon} style={{cursor: 'pointer'}}/></div>
+					<div className="col-md-2"
+						onClick={() => this.closeReportCallIssue()}>
+						<SVG src={closeOrDismissIcon} style={{cursor: 'pointer'}}/></div>
 				</div>
-				<div className="row mt-2">
+				<div className="row mt-0">
 					<div className="col-md-12">
 						<a className="text-left"
 						   style={{fontFamily: 'AmazonEmber', fontSize: '12px', color: '#000000'}}>How was the call
@@ -48,7 +58,8 @@ class Body extends Component {
 						{
 							feedbackRatings.map((currentFeedback,) => (
 								<a key={`feedback-rating-${currentFeedback}`}
-								   style={{cursor: 'pointer'}}>
+								   style={{cursor: 'pointer'}}
+								   onClick={() => this.feedbackChange(currentFeedback)}>
 									<SVG src={currentFeedback <= feedback ? starYellowIcon : starWhiteIcon}/>
 								</a>
 							))
@@ -59,7 +70,7 @@ class Body extends Component {
 						fontSize: '12px',
 						letterSpacing: 'normal',
 						color: '#000000'
-					}}>Good
+					}}>{lo.get(feedbackRatingsText, feedback - 1)}
 					</div>
 				</div>
 				<div className="row">
@@ -80,18 +91,83 @@ class Body extends Component {
 					}}>3 selected
 					</div>
 				</div>
+
+				<div>
+					<div className="row mt-1 ml-0 mr-0" style={{
+						maxHeight: '117px',
+						minHeight: '20%',
+						border: 'solid 1px #cfcfcf',
+						backgroundColor: '#f7f7f7',
+						overflowY: 'scroll'
+					}}>
+						<div className="col-md-12 mt-1" style={{}}>
+							<div className="row">
+								<div className="col-md-12">
+									<a style={{fontFamily: 'AmazonEmber', fontSize: '14px', color: '#000000'}}>
+										<SVG src={markFalseIcon}/> I need help, contact
+										me</a>
+								</div>
+							</div>
+							<div className="row">
+								<div className="col-md-12">
+									<a style={{
+										opacity: '0.6',
+										fontFamily: 'AmazonEmber',
+										fontSize: '12px',
+										letterSpacing: 'normal',
+										color: '#000000'
+									}}> Audio related problems </a>
+								</div>
+							</div>
+							<div className="row">
+								<div className="col-md-12">
+									<a style={{fontFamily: 'AmazonEmber', fontSize: '14px', color: '#000000'}}>
+										<SVG src={markTrueIcon}/> Customer can't
+										hear agent</a>
+								</div>
+							</div>
+							<div className="row">
+								<div className="col-md-12">
+									<a style={{fontFamily: 'AmazonEmber', fontSize: '14px', color: '#000000'}}>
+										<SVG src={markFalseIcon}/> No audio</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="row mt-3">
+						<div className="col-md-12">
+							<textarea placeholder="Enter your notes about the issue here" rows={3}
+									  className="report-error-user-note col-md-12" style={{
+								maxHeight: '55px',
+								minHeight: '10px',
+								backgroundColor: '#f7f7f7',
+								resize: 'none',
+								fontFamily: 'AmazonEmber',
+								fontSize: '14px',
+								color: '#000000'
+							}} defaultValue={""}/>
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
 }
 
 Body.propTypes = {
+	feedback: PropTypes.number.isRequired,
+	feedbackChange: PropTypes.func.isRequired,
 	closeReportCallIssue: PropTypes.func.isRequired,
 	requestReportACallIssue: PropTypes.func.isRequired,
 };
-const mapStateToProps = state => ({});
+const mapStateToProps = state => ({
+	feedback: state.acReducer.feedback || defaultFeedback,
+});
 
 const mapDispatchToProps = dispatch => ({
+	feedbackChange: (feedback) => {
+		dispatch(onFeedbackChange(feedback));
+	},
 	closeReportCallIssue: () => {
 		dispatch(onRequestReportCallIssue('close'));
 	},
