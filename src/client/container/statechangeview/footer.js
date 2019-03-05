1acccabc7dcbcb87dbc90765ea2b3c792a1052a2
pallab-gain <pallab@callstats.io>
2019-03-05 13:19:49 +0200

    Hidden iframe to handle logged out.

--- src/client/container/statechangeview/footer.js
+++ src/client/container/statechangeview/footer.js
@@ -1,19 +1,35 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
+import PropTypes from "prop-types";
 
+
+const logoutURL = 'https://callstatsio.awsapps.com/connect/logout';
 
 class Footer extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			loggedOut: false,
+		};
+	}
+
+	logout() {
+		this.setState({
+			loggedOut: true
+		});
+		window.location.reload();
 	}
 
 	render() {
 		return (
 			<div className="card-footer" style={{backgroundColor: 'inherit', marginRight: '5%', marginLeft: '5%'}}>
 				<div className="row">
-					<div className="col-md-12">
-						<a style={{fontFamily: 'AmazonEmber', color: '#3885de'}} href="#">Log out</a>
+					<div className="col-md-12" onClick={() => this.logout()} style={{cursor: 'pointer'}}>
+						<a style={{fontFamily: 'AmazonEmber', color: '#3885de'}}>Log out</a>
 					</div>
+				</div>
+				<div style={{display: 'none'}}>
+					{this.state.loggedOut && <iframe src={logoutURL}></iframe>}
 				</div>
 			</div>
 		);
