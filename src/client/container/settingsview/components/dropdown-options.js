ba5cead40590eda1dea0e2b73d63cb8f6b6efcdc
pallab-gain <pallab@callstats.io>
2019-03-26 10:03:02 +0200

    Take at most 30 letter from the drop down. truncate others.

--- src/client/container/settingsview/components/dropdown-options.js
+++ src/client/container/settingsview/components/dropdown-options.js
@@ -1,14 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
-
+import lo from 'lodash';
 import styles from './../settings.css';
 
+const truncate = (sentence = "", upto = -1) => {
+	if (upto < 0) {
+		return sentence;
+	}
+	if (sentence.length < upto) {
+		return sentence;
+	}
+
+	return `${sentence.substring(0, upto - 4)} ...`;
+};
 const DropDownOptions = ({toggleMenuItem, changeAudioInputDevice, showMenuItem, inputDeviceList, audioInputDevice}) => (
 	<div className="col-md-10 pr-0 mr-0">
 		<div className={`btn-group ${styles.btnGroup}`}>
-			<button className={`btn ${styles.dropDownButton}`}
+			<button className={`btn text-left ${styles.dropDownButton}`}
 					type="button"
-					onClick={toggleMenuItem}> {audioInputDevice.label}
+					onClick={toggleMenuItem}> {truncate(audioInputDevice.label, 30)}
 			</button>
 
 			<button onClick={toggleMenuItem} type="button"
