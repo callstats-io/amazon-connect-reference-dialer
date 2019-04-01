17fe8362b00b35cb8884876b98edde7c60ba818e
pallab-gain <pallab@callstats.io>
2019-04-01 21:46:22 +0300

    Update loader.

--- src/client/container/connectivitycheckview/loading/loading.js
+++ src/client/container/connectivitycheckview/loading/loading.js
@@ -2,12 +2,14 @@ import React from "react";
 import styles from './loading.css';
 
 const Loading = ({}) => (
-	<span className={`${styles["lds-ellipsis"]} w-100`}>
-		<div></div>
-		<div></div>
-		<div></div>
-		<div></div>
-	</span>
+    <span className={styles.showbox}>
+        <div className={styles.loader}>
+            <svg className={styles.circular} viewBox="25 25 50 50">
+                <circle className={styles.path} cx="50" cy="50" r="20" fill="none" strokeWidth="5"
+                        strokeMiterlimit="10"/>
+            </svg>
+        </div>
+    </span>
 );
 
 export default Loading;
