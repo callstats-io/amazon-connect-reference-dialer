11e47a0b5c863f6b45d215c668c7eba9d98b3dd7
pallab-gain <pallab@callstats.io>
2019-04-07 21:14:03 +0300

    Fix for mute-unmute icon.

--- src/client/container/agentview/conferenceconnection/components/togglemute.js
+++ src/client/container/agentview/conferenceconnection/components/togglemute.js
@@ -2,23 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from './../agentview.css';
 
-import muteIcon from '../../../../res/images/muted-icon.svg';
-import unMuteIcon from '../../../../res/images/fa-mic.svg';
-
+import muteIcon from './../../../../res/images/fa-mute.svg';
+import unMuteIcon from './../../../../res/images/fa-mic.svg';
 
 const MuteUnmute = ({toggleMuteUnmute, muted = false, marginTop = ""}) => (
-	<div className={`col-md-6 ${marginTop}`}>
-		<a className={`btn ${styles.buttonCommon}`}
-		   onClick={toggleMuteUnmute}
-		>
-			<img src={muted ? unMuteIcon : muteIcon}/> {muted ? ' Unmute' : ' Mute'}
-		</a>
-	</div>
+    <div className={`col-md-6 ${marginTop}`}>
+        <a className={`btn ${styles.buttonCommon}`}
+           onClick={toggleMuteUnmute}>
+            {muted ? <img src={unMuteIcon}/> : <img src={muteIcon}/>}
+            {muted ? ' Unmute' : ' Mute'}
+        </a>
+    </div>
 );
 MuteUnmute.propTypes = {
-	muted: PropTypes.bool,
-	toggleMuteUnmute: PropTypes.func,
-	marginTop: PropTypes.string
+    muted: PropTypes.bool,
+    toggleMuteUnmute: PropTypes.func,
+    marginTop: PropTypes.string
 };
 
 export default MuteUnmute;
