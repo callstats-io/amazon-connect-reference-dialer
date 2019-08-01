436f3f66456a006bbc001a6593f193607128d812
pallab-gain <pallab@callstats.io>
2019-08-01 17:20:21 +0300

    Fix custom dialer issue. Memorize last used country

--- src/client/container/dialerview/body.js
+++ src/client/container/dialerview/body.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import ReactPhoneInput from './phoneinput/csio.reactphone.input';
+import ReactPhoneInput from 'react-phone-input-2';
 import lo from 'lodash';
 import closeOrDismissIcon from '../../res/images/fa-close-or-dismiss.svg';
 
@@ -13,9 +13,6 @@ import styles from './dialpad.css';
 
 import sessionManager from './../../api/sessionManager';
 import DialPad from './dialpad';
-import {
-  getCountryDetails, getFormattedPhoneNumber, isPhoneNumber
-} from './phoneinput/utils';
 import databaseManager from '../../api/databaseManager';
 
 class Body extends Component {
@@ -23,39 +20,48 @@ class Body extends Component {
     super(props);
     this.state = {
       phoneNumber: '',
-      phoneNumber2: '',
-      selectedCountry: getCountryDetails(databaseManager.getDefaultCountry() || lo.first(sessionManager.getDialableCountries())),
-      dialableCountries: sessionManager.getDialableCountries().map(currentCountry => getCountryDetails(currentCountry))
+      defaultCountry: databaseManager.getDefaultCountry() || lo.first(sessionManager.getDialableCountries(), 'fi'),
+      errorCount: 0
     };
+    this.handleInputChange = this.handleInputChange.bind(this);
     this.numPadHandler = this.numPadHandler.bind(this);
-    this.onCountrySelected = this.onCountrySelected.bind(this);
-    this.onPhoneNumberChange = this.onPhoneNumberChange.bind(this);
+    this.onClickHandler = this.onClickHandler.bind(this);
+    this.inputValue = React.createRef();
   }
 
   closeDialPad () {
     this.props.closeDialPad();
   }
 
+  handleInputChange (value, countryData) {
+    if (this.state.defaultCountry !== lo.get(countryData, 'countryCode')) {
+      databaseManager.setDefaultCountry(lo.get(countryData, 'countryCode'));
+    }
+    this.setState({
+      phoneNumber: value
+    });
+    if (sessionManager.isActive()) {
+      sessionManager.sendDigit(lo.last(value)).then(success => {
+      }).catch(err => console.warn(err));
+    }
+  }
+
   numPadHandler (value) {
     if (!value) {
       return;
     }
+    const { phoneNumber } = this.state;
+    this.setState({
+      phoneNumber: `${phoneNumber}${value}`
+    });
     if (sessionManager.isActive()) {
       sessionManager.sendDigit(value).then(success => {
       }).catch(err => console.warn(err));
     }
-    let { phoneNumber2: number } = this.state;
-    const phoneNumber = `${number}${value}`;
-    if (!isPhoneNumber(phoneNumber)) {
-      return;
-    }
-    this.setState({
-      phoneNumber2: phoneNumber
-    });
   }
 
   dialNumber () {
-    const { phoneNumber2: phoneNumber } = this.state;
+    const { formattedNumber: phoneNumber } = this.inputValue.current.state;
     sessionManager.dialNumber(phoneNumber).then(success => {
       this.closeDialPad();
     }, err => {
@@ -63,30 +69,15 @@ class Body extends Component {
     });
   }
 
-  onCountrySelected (selectedCountry) {
-    const phoneNumber = getFormattedPhoneNumber(this.state.dialableCountries, selectedCountry, this.state.phoneNumber2);
+  // eslint-disable-next-line handle-callback-err
+  componentDidCatch (error, info) {
     this.setState({
-      selectedCountry: selectedCountry,
-      phoneNumber2: isPhoneNumber(phoneNumber) ? phoneNumber : ''
-    });
-    databaseManager.setDefaultCountry(lo.get(selectedCountry, 'iso2'));
-  }
-
-  onPhoneNumberChange (currentPhoneNumber) {
-    const phoneNumber = lo.get(currentPhoneNumber, 'target.value', '');
-    if (sessionManager.isActive()) {
-      sessionManager.sendDigit(lo.last(phoneNumber)).then(success => {
-      }).catch(err => console.warn(err));
-    }
-    if (!isPhoneNumber(phoneNumber)) {
-      return;
-    }
-    this.setState({
-      phoneNumber2: phoneNumber
+      errorCount: this.state.errorCount + 1
     });
   }
 
   render () {
+    const dialableCountries = sessionManager.getDialableCountries();
     return (
       <div className="card-body" style={{ backgroundColor: '#ffffff' }}>
         <div className="row">
@@ -99,14 +90,20 @@ class Body extends Component {
           </div>
         </div>
         <div className="row mt-2">
-          <div className="col-md-9 pr-0">
+          <div className="col-md-9">
             <ReactPhoneInput
-              phoneNumber={this.state.phoneNumber2}
-              onPhoneNumberChange={this.onPhoneNumberChange}
-              selectedCountry={this.state.selectedCountry}
-              dialableCountryList={this.state.dialableCountries}
-              onCountrySelected={this.onCountrySelected}
-            />
+              ref={this.inputValue}
+              inputStyle={{ minWidth: '15.5em', maxWidth: '15.5em', boxShadow: 'none', borderRadius: '0' }}
+              onlyCountries={dialableCountries}
+              defaultCountry={this.state.defaultCountry}
+              enableSearchField={true}
+              value={this.state.phoneNumber}
+              inputExtraProps={{
+                name: 'phone',
+                required: true,
+                autoFocus: true
+              }}
+              onChange={ this.handleInputChange }/>
           </div>
           <div className="col-md-3 p-0 m-0">
             <a className={`btn ${styles.dialButton}`}
