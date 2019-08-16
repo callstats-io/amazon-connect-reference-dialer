5771aa3d1f50bdb00af743209bd707a46c5f5db8
Pallab <pallab.gain@gmail.com>
2019-08-16 13:57:53 +0300

    Truncate long names and show full names in hover.

--- src/client/container/quickconnects/contactfield.js
+++ src/client/container/quickconnects/contactfield.js
@@ -3,26 +3,54 @@ import PropTypes from 'prop-types';
 
 import styles from './quickconnects.css';
 
+import {
+  truncate
+} from '../../utils/acutils';
+
+class EachContact extends React.Component {
+  constructor (props) {
+    super(props);
+    this.tooltipRef = React.createRef();
+  }
+  componentDidMount () {
+    $(this.tooltipRef.current).tooltip();
+  }
+  render () {
+    return (
+      <div className={`row ${styles.contactItem}`}>
+        <div className={`col-8 pl-1 pr-0 ml-0 mr-0`}>
+          <a className={`btn pl-0 ml-0 pr-0 mr-0 ${styles.contactName}`}
+            ref={this.tooltipRef}
+            title={this.props.currentContact.name}
+            data-placement="top">
+            { truncate(`${this.props.currentContact.name}`, 22) }
+          </a>
+        </div>
+        <div className={`col-4 text-right pr-0`}>
+          <a className={`btn ${styles.dialButton}`} onClick={() => this.props.dialContact(this.props.currentContact)}>
+            Dial
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+EachContact.propTypes = {
+  currentContact: PropTypes.object.isRequired,
+  dialContact: PropTypes.func.isRequired
+};
+
 const ContactField = ({ contactList = [], dialContact }) => (
   <div className={`row ml-0 mr-0 mt-2 ${styles.contactListBox}`}>
     <div className={`col-12`}>
-      {
-        contactList.map((currentContact, index) => (
-          <div className={`row ${styles.contactItem}`} key={`contact-list-${index}`}>
-            <div className={`col-8`}>
-              <a className={`btn pl-0 ml-0 ${styles.contactName}`}>
-                {currentContact.name}
-              </a>
-            </div>
-            <div className={`col-4 text-right pr-0`}>
-              <a className={`btn ${styles.dialButton}`} onClick={() => dialContact(currentContact)}>
-								Dial
-              </a>
-            </div>
-          </div>
-        ))
+      { contactList.map((currentContact, index) => (
+        <EachContact
+          key={`contact-list-${index}`}
+          currentContact={currentContact}
+          dialContact={dialContact}/>
+      ))
       }
-
     </div>
   </div>
 );
