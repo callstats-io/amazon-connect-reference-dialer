808b1f6d32e914977d1597f6c477477e6c0f40af
Pallab <pallab.gain@gmail.com>
2019-10-14 16:11:50 +0300

    Enable call reporting only after the call.

--- src/client/container/footer/components/reportcall.js
+++ src/client/container/footer/components/reportcall.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import acManager from '../../../api/acManager';
+import { isAfterCallWork } from '../../../utils/acutils';
+import sessionManage from '../../../api/sessionManager';
 
+// take existing class and add disabled is it is not after call work
+const getClass = (prvClass = '') => {
+  const currentState = acManager.getCurrentState();
+  const primaryState = sessionManage.getCurrentStateString(currentState, true);
+  const secondaryState = sessionManage.getCurrentStateString(currentState, false);
+  if (isAfterCallWork(primaryState) || isAfterCallWork(secondaryState)) {
+    return prvClass;
+  }
+  return `${prvClass} disabled`;
+};
 const ReportACall = ({ id = '', divClass = '', linkClass = '', style = {}, onClickHandler }) => (
   <div className={divClass}>
     <a id={id}
-      className={linkClass}
+      className={getClass(linkClass)}
       style={style}
       onClick={onClickHandler}
     > Report a call issue </a>
